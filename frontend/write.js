68780ea4bdd8a48b57f55d46b7769d4011463e41
aw0605 <101087917+aw0605@users.noreply.github.com>
2023-07-05 01:04:49 +0900

    글쓰기 페이지 기능 구현
    - 프론트엔드 글쓰기 페이지를 form으로 작업
    - 백엔드와 SQLite를 연동해서 이미지 및 데이터를 업로드하는 기능 구현

---
+++ frontend/write.js
@@ -0,0 +1,19 @@
+async function handleSubmitForm(e) {
+  e.preventDefault();
+  const body = new FormData(form);
+  body.append("insertAt", new Date().getTime());
+  try {
+    const res = await fetch("/items", {
+      method: "POST",
+      body,
+    });
+    const data = await res.json();
+    if (data === "200") window.location.pathname = "/";
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+const form = document.getElementById("write-form");
+
+form.addEventListener("submit", handleSubmitForm);
