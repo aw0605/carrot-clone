3d0e8febf6e45ef857926fe0cc4adf21152a720c
aw0605 <101087917+aw0605@users.noreply.github.com>
2023-07-06 01:38:56 +0900

    회원가입 로직 기능 구현
    - 프론트엔드 및 백엔드 작업(signup)

--- main.py
+++ main.py
@@ -56,5 +56,23 @@ async def get_image(item_id):
     
     return Response(content=bytes.fromhex(image_bytes), media_type='image/*')
 
+@app.post('/signup')
+def signup(id:Annotated[str,Form()], 
+           password:Annotated[str,Form()],
+           name:Annotated[str,Form()],
+           email:Annotated[str,Form()]):
+    cur.execute(f"""
+                SELECT id FROM users WHERE id= '{id}'
+                """)
+    existing_user = cur.fetchone()
+    if existing_user:
+        return "이미 회원가입이 되어있습니다."
+    cur.execute(f"""
+                INSERT INTO users(id, name, email, password)
+                VALUES('{id}','{name}','{email}','{password}')
+                """)
+    con.commit()
+    return "200"
+
 app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
 
